23eff8e305e98c5a8a26a962bdb0f3667de935c7
rahim-uddin-jsr <rahimuddin5915@gmail.com>
2024-05-11 12:11:47 +0600

    user login registration done

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        // User::factory()->create([
+        //     'name' => fake()->name(),
+        //     'email' => 'admin@gmail.com',
+        //     'role' => 'admin',
+        //     'status' => 1,
+        //     'email_verified_at' => now(),
+        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //     'remember_token' => Str::random(10),
+        // ]);
+
+        // User::factory(10)->create();
+    }
+}
